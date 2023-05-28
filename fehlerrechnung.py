575fc7ff24be3ad918bfbf9d8a69959214adf52d
Jan Lucca Viola <janlucca.viola@tu-dortmund.de>
2023-05-28 12:41:17 +0200

    Durchführung und Aufbau

--- fehlerrechnung.py
+++ fehlerrechnung.py
@@ -11,7 +11,7 @@ from uncertainties.unumpy import (nominal_values as noms,   # Wert:             
                                   std_devs as stds)  
 
 
-nu = uarray ([2540,2607,3002,2504,2519,2836,2596,2664,2967],[0,0,0,0,0,0,0,0,0])
+nu = uarray ([2540,2607,2117,3002,2504,2519,2836,2596,2664,2967],[0,0,0,0,0,0,0,0,0,0])
 
 ud = uarray(5.00*10**(-3),0.01*10**(-3))
 
@@ -23,5 +23,8 @@ print(n)                            # Anzahl der Daten
 mean = sum(nu)/n  
 print(mean)                   # Mittelwert
 sigma = (sum((nu - mean)**2)/n-1)**(1/2) 
+m= sum(Lambda(ud,nu))/len(Lambda(ud,nu))
+
 print(sigma)
 print('Lambda', Lambda(ud,nu))
+print('Mittelwert',Lambda(ud,mean))
